b5c1e3fe82773043e1500a2f6ecf5c5b6082c84d
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-03-10 11:29:44 -0300

    Continuação do primeiro projeto de revisão

---
+++ RevisaoJava/src/br/uninove/telas/Principal.java
@@ -0,0 +1,28 @@
+package br.uninove.telas;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Primeira Palavra!");
+        System.out.println("Segunda Palavra");
+        System.out.println("Terceira\n\tQuarta");
+        System.out.println("Palavra \"entre aspas!\"");
+        System.out.println("Exemplo de \"barra\": \"");
+        
+        /*Desafio do tio:
+            Crie um programinha (ou edite este) que mostre seus
+        dados (todos fixos, uma impressão por linha), exemplo:
+   
+        Nome: Thiago Graziani Traue
+            Idade: 35
+            Profissão: Professor do Ens. Superior
+            Hobbies:
+                - Fotografia
+                - Video Game
+                - Maratonar Séries
+            Frase de efeito: "Não usem dorgas!"
+        */
+        
+    }
+}
